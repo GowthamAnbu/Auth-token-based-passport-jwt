765ef5bd90614df199f92f55715a5b20286b95c1
Gowtham Anbazhagan <gowthama@tag086.tag.dc>
2017-11-21 12:49:51 +0530

    rename file using multer

--- config/routes.js
+++ config/routes.js
@@ -3,10 +3,18 @@ const users = require('../controllers/users'),
       auth = require('./auth'),
       passport = require('passport'),
       mongoose = require('mongoose');
+      const path = require('path');
       const multer = require('multer');
-      const upload = multer({
-        dest: 'uploads/' // this saves your file into a directory called "uploads"
-      }); 
+      let storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+          cb(null, './uploads/images')
+        },
+        filename: function (req, file, cb) {
+          cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+        }
+      });
+      const upload = multer({storage:storage}).single("profile-photo");
+      const Jimp = require("jimp"); 
 module.exports = (app) => {
     
     app.use(cors());
@@ -20,8 +28,31 @@ module.exports = (app) => {
       });
       
       // It's very crucial that the file name matches the name attribute in your html
-      app.post('/', upload.single('file-to-upload'), (req, res) => {
-        res.redirect('/');
+      app.post('/',(req, res) => {
+          upload(req, res, (err)=>{
+            if (err) {
+                console.log(err.message);
+                res.send("erro saving file");
+              }
+              console.log(req.file);
+              console.log("fine");
+              res.redirect('/');
+          })
+          /* console.log(req.file.path);
+          console.log(req.file.originalname);
+          console.log(req.file.originalname.lastIndexOf('.'));
+          console.log(req.file.originalname.slice(req.file.originalname.lastIndexOf('.'))); */
+          
+          /* Jimp.read("./images/techaffinity-squarelogo-1466416896536.png", function (err, squarelogo) {
+            if (err) throw err;
+            squarelogo.resize(256, 256)            // resize 
+                 .quality(100)                 // set JPEG quality 
+                 .write("./images/small/techaffinity-squarelogo-1466416896536-sm.png",function(err,squarelogo){
+                     if(err) throw err;
+                     console.log("file successfully written");
+                 }); // save 
+                 console.log("success");
+            }); */
       });
       
     app.post("/register",auth.register);
