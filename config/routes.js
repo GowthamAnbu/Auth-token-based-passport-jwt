db1935a4f8bfdb0a0aafe0590aa1195ff83a8883
Gowtham Anbazhagan <gowthama@tag086.tag.dc>
2017-11-21 13:37:17 +0530

    added jimp for image proccessing

--- config/routes.js
+++ config/routes.js
@@ -27,32 +27,48 @@ module.exports = (app) => {
         res.sendFile(__dirname + '/index.html');
       });
       
-      // It's very crucial that the file name matches the name attribute in your html
       app.post('/',(req, res) => {
           upload(req, res, (err)=>{
             if (err) {
                 console.log(err.message);
                 res.send("erro saving file");
               }
-              console.log(req.file);
-              console.log("fine");
+            //   console.log(req.file.filename);
+            //   console.log(req.file.path);
+              let smallUrl =req.file.path.slice(0,req.file.path.lastIndexOf("/"))+"/small/"
+              +req.file.filename.slice(0,req.file.filename.lastIndexOf('.'))+ "-sm"+path.extname(req.file.filename); 
+              
+              let mediumUrl =req.file.path.slice(0,req.file.path.lastIndexOf("/"))+"/medium/"
+              +req.file.filename.slice(0,req.file.filename.lastIndexOf('.'))+ "-md"+path.extname(req.file.filename); 
+
+              let largeUrl =req.file.path.slice(0,req.file.path.lastIndexOf("/"))+"/large/"
+              +req.file.filename.slice(0,req.file.filename.lastIndexOf('.'))+ "-lg"+path.extname(req.file.filename); 
+              
+            //   console.log(req.file.filename.slice(0,req.file.filename.lastIndexOf('.'))+ "-sm"+path.extname(req.file.filename) )
+                Jimp.read(req.file.path, function (err, profilePhoto) {
+                    if (err) throw err;
+                    profilePhoto.resize(100, 75)
+                        .quality(100)
+                        .write(smallUrl,function(err,profilePhoto){
+                            if(err) throw err;
+                            console.log("resized 100*75");
+                        });
+                    profilePhoto.resize(460, 320)
+                        .quality(100)
+                        .write(mediumUrl,function(err,profilePhoto){
+                            if(err) throw err;
+                            console.log("resized 460*320");
+                        });
+                    profilePhoto.resize(520, 500)
+                        .quality(100)
+                        .write(largeUrl,function(err,profilePhoto){
+                            if(err) throw err;
+                            console.log("resized 520*500");
+                        });
+                });
+              console.log("file saved Successfully");
               res.redirect('/');
           })
-          /* console.log(req.file.path);
-          console.log(req.file.originalname);
-          console.log(req.file.originalname.lastIndexOf('.'));
-          console.log(req.file.originalname.slice(req.file.originalname.lastIndexOf('.'))); */
-          
-          /* Jimp.read("./images/techaffinity-squarelogo-1466416896536.png", function (err, squarelogo) {
-            if (err) throw err;
-            squarelogo.resize(256, 256)            // resize 
-                 .quality(100)                 // set JPEG quality 
-                 .write("./images/small/techaffinity-squarelogo-1466416896536-sm.png",function(err,squarelogo){
-                     if(err) throw err;
-                     console.log("file successfully written");
-                 }); // save 
-                 console.log("success");
-            }); */
       });
       
     app.post("/register",auth.register);
